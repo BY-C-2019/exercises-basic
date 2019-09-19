3de42d72e31ca95ffa8ddbb513603ae7f30501c4
ellensdogos <ellen.sdogos@hotmail.com>
2019-09-19 14:05:44 +0200

    for-loop övning

---
+++ exercise2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int input = 40; input > 29; input --){
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
