2389d59646fa62177312f97788df1e45ee847cd0
Lukas Selin <lukas.selin@viskan.se>
2019-09-19 15:16:41 +0200

    excerise5 done

---
+++ metoder/excerise5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace excerise5
+{
+    class Program
+    {
+            static double AddMoms(double sumBeforeMoms, int momsSatsen)
+        {
+            double moms = (momsSatsen / 100) +1;
+            return sumBeforeMoms * moms;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
