3b63231d184256f6b0443b30f44b4edcc8b8fba4
Lukas Selin <lukas.selin@viskan.se>
2019-09-19 15:17:07 +0200

    excerise 5 fixed

--- metoder/excerise5/Program.cs
+++ metoder/excerise5/Program.cs
@@ -4,10 +4,11 @@ namespace excerise5
 {
     class Program
     {
-            static double AddMoms(double sumBeforeMoms, int momsSatsen)
+            static double AddMoms(double sumBeforeMoms, double momsSatsen)
         {
-            double moms = (momsSatsen / 100) +1;
-            return sumBeforeMoms * moms;
+            double moms = (momsSatsen / 100.0) +1.0;
+            double finalSum = sumBeforeMoms * moms;
+            return finalSum;
         }
         static void Main(string[] args)
         {
