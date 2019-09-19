0baad1f0c0d50134178193c4b75c0d1970ec7a46
Karin Norrlander <karinnorrlander@karins-mbp.simonsland.local>
2019-09-19 14:57:40 +0200

    decimaltal

---
+++ Exersise3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exersise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //omvandla decimal till heltal
+            Console.WriteLine("Det här programmet omvandlar decimaltal till heltal. Skriv in ett decimaltal, tack!");
+            string input = Console.ReadLine();
+            float decimaltal = float.Parse(input);
+            int heltal = (int)Math.Round(decimaltal);
+            Console.WriteLine("Det avrundade talet är: " + heltal);
+        }
+    }
+}
