6703a5e452c06b4ff1965bbb4acb60b1efd099fb
Lukas Selin <lukas.selin@viskan.se>
2019-09-19 14:00:27 +0200

    exercise 1 (methods)

---
+++ metoder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace metoder
+{
+    class Program
+    {
+        static string Hello(string input){
+            string output = input + " World!";
+                
+            return output;
+        }
+        static void Main(string[] args)
+        {
+            string string1 = Console.ReadLine();
+            string outputString = Hello(string1);
+            
+            Console.WriteLine(outputString);
+        }
+    }
+}
