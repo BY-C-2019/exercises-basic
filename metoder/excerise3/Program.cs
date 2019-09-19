0001cb739024bc9eaf67409956fb4e279b4be061
Lukas Selin <lukas.selin@viskan.se>
2019-09-19 15:03:07 +0200

    excerise3 done

---
+++ metoder/excerise3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace excerise3
+{
+    class Program
+    {
+        /* Skapa metoden Addera() som tar in två heltal och returnerar summan. */
+        static int Addera(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
