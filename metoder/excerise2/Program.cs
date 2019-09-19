04a4bdd8ed87be7d9eea64fca1f8f341d9617332
Lukas Selin <lukas.selin@viskan.se>
2019-09-19 14:40:18 +0200

    Excerise2 done

---
+++ metoder/excerise2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace excerise2
+{
+    class Program
+    {
+        static string AddTwoStrings(string firstString, string secondString)
+        {
+            string outputString = firstString + secondString;
+
+            return outputString;
+        }
+        static void Main(string[] args)
+        {
+            /* Skapa en metod som tar in två string variabler, lägger samman dessa och returnerar som en sträng.
+             (Tex. Strängarna ”Hej på dig!” och ” Hur mår du?” blir ”Hej på dig! Hur mår du?”. */
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
