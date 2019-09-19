f794fdcbeaba5ee161e65d927090fbaabcb60579
Filip Larsson <filip19911212@gmail.com>
2019-09-19 14:34:04 +0200

    string array for loop

---
+++ exercises2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace exercises2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = new string[5];
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.Write("\nSkriv in ett namn: ");
+                name[i] = Console.ReadLine();
+
+            }
+            Console.Clear();
+            Console.WriteLine("\nLaddar.");
+            Thread.Sleep(700);
+            Console.Clear();
+            Console.WriteLine("\nLaddar...");
+            Thread.Sleep(700);
+            Console.Clear();
+            Console.WriteLine("\nLaddar....");
+            Thread.Sleep(700);
+            Console.Clear();
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine("\n" + name[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
