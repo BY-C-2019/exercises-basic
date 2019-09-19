03fb7b05ebaca065a25e47fe014b027d7310346c
wiseby <l.wiseby@gamil.com>
2019-09-19 14:30:55 +0200

    exercise2 finished

---
+++ exercise2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double decimalNumber = 0;
+            int integer = 0;
+            Console.Write(">>");
+            try
+            {
+                decimalNumber = Convert.ToDouble(Console.ReadLine());
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Oppss! Ett fel inträffade!");
+                Console.WriteLine("Felmeddelande: " + e);
+            }
+
+            integer = Convert.ToInt32(decimalNumber);
+
+
+            Console.WriteLine("Decimaltal är: " + decimalNumber);
+            Console.WriteLine("Avrundat tal är: " + integer);
+            Console.ReadKey();
+        }
+    }
+}
