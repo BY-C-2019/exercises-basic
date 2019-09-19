951b98a247f6e512b2af01bfc4d381e6f9f62172
ellensdogos <ellen.sdogos@hotmail.com>
2019-09-19 14:55:28 +0200

    övning 5

---
+++ exercise5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vad heter du? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Välkommen " + name);
+
+            Console.Write("Hur gammal är du? ");
+            string input = Console.ReadLine();
+
+            int age = Convert.ToInt32(input);
+            int daysYouHaveLived = age * 365;
+
+            Console.WriteLine("Du har levt i " + daysYouHaveLived + " dagar! ");
+        }
+    }
+}
