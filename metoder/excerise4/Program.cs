f6b42bd2fbb31313358c2a69489261828b3d82c0
Lukas Selin <lukas.selin@viskan.se>
2019-09-19 15:07:30 +0200

    exceruse4 done

---
+++ metoder/excerise4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace excerise4
+{
+    class Program
+    {
+        static double AddMoms(double sumBeforeMoms)
+        {
+            return sumBeforeMoms * 1.25;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
