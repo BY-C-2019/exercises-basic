b43ee04b8e540da5ae8ed98ad22f621f2c2e6ea0
wiseby <l.wiseby@gamil.com>
2019-09-19 15:10:38 +0200

    Battleship game visualized

---
+++ BattleShipCLI/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace BattleShipCLI
+{
+    class Program
+    {
+        /* GameBoard layout:
+        *         0   1   2   3   4   5
+        *   >   -------------------------
+        *   0   | * |   |   |   | # |   |
+        *   >   -------------------------
+        *   1   | * |   |   |   | # |   |
+        *   >   -------------------------
+        *   2   |   |   |   |   | # |   |
+        *   >   -------------------------
+        *   3   |   |   | * | * | * |   |
+        *   >   -------------------------
+        *   4   |   | ~ |   |   |   |   |
+        *   >   -------------------------
+        *   5   |   |   |   |   |   |   |
+        *   >   -------------------------
+        *
+        *       Hit = #
+        *       Miss = ~
+        *       Ship = *
+        *
+        *       
+        */
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            int boardSizeX = 10;
+            int boardSizey = 10;
+
+            char[,] gameBoard = new char[,]
+
+
+            Console.WriteLine("All personell to battlestation!");
+            Console.WriteLine("It's time for BattleShip!");
+            Console.WriteLine("In the Command Line Interface!");
+            Console.WriteLine("================================");
+
+        }
+    }
+}
