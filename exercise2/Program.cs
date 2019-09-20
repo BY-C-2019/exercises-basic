ec6a8bdbdcf38eaa2496d2ca242b01ff7c25b1ae
lyret <viktor@lyresten.se>
2019-09-20 09:15:51 +0200

    löser uppgift 2

---
+++ exercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				Console.Write("Ange ett decimaltal:");
+				string input = Console.ReadLine();
+				double value = Convert.ToDouble(input);
+				int closestInt = (int)Math.Round(value);
+				Console.WriteLine("Närmaste heltalet var: " + closestInt);
+			}
+			catch
+			{
+				Console.WriteLine("Snälla, du vet ju hur ett decimaltal ser ut 🤦‍");
+			}
+        }
+    }
+}
