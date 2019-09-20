a1031ebb613b10c9d30aafc3e031c32eee93007b
Karin Norrlander <karin@barprego.se>
2019-09-20 10:33:45 +0200

    Vilket väder?

---
+++ EXERCISE5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EXERCISE5
+{
+    
+    class Program
+    {
+        //Fråga om det är fint väder
+        //Om användaren svarar j – vi går på picknik
+
+    
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Är det fint väder? Svara j eller n");
+            string svar;
+            svar = Console.ReadLine().ToLower();
+
+            if(svar =="j")
+            {
+                Console.WriteLine("Vi går på picknick");
+            }
+            else if (svar == "n")
+            {
+                Console.WriteLine("Vi stannar inne och läser en bok!");
+            }
+            //Om användaren matar in något annat än n,N,j,J – programmet skriver ut "Jag förstår inte"
+            if(svar != "j" || svar!= "n")
+            {
+                Console.WriteLine("Jag förstår inte!");
+            }
+        }
+    }
+}
