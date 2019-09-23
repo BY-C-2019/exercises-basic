67bcf700a6e2066eac92fb7129d3dd3cb1cc1c49
wiseby <l.wiseby@gamil.com>
2019-09-23 09:13:07 +0200

    Exercise5 Done

---
+++ exercise5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int age;
+            int daysAlive;
+
+            Console.WriteLine("Hej! Vad heter du?");
+            Console.Write(">>");
+            input = Console.ReadLine();
+            Console.WriteLine("Hej " + input + ". Hur gammal är du?");
+            age = Convert.ToInt32(Console.ReadLine());
+            daysAlive = age * 365;
+            Console.WriteLine("Du har levt i ungefär " + daysAlive + " dagar.");
+            Console.ReadKey();
+        }
+    }
+}
