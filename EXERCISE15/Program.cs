75e0d610d1169fb6bc78b1fa54dfc6174b2f821b
Karin Norrlander <karin@barprego.se>
2019-09-23 13:31:44 +0200

    löser uppgift 16

---
+++ EXERCISE15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EXERCISE15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdderaText("Hej", "på dig");
+            // Console.Write("Skriv in den första texten:");
+            // string a = Console.ReadLine();
+            // Console.Write("Skriv in den andra texten:");
+            // string b = Console.ReadLine();
+            // Console.WriteLine(Addera(string a, string b));
+            
+        }
+
+        static string AdderaText(string a, string b) 
+        {
+            //Två texter ska vara indata
+            //Metoden ska returnera en string där dessa två texter är sammanslagna
+            //De båda texterna skrivs ut tillsammans
+            string nyText = a + b;
+            return nyText; 
+            Console.WriteLine(nyText);
+        }
+    }
+}
---
+++ EXERCISE16/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EXERCISE16
+{
+    class Program
+    {
+        //Skapa metoden Addera() som tar in två heltal och returnerar summan.
+        static void AdderaSumma(int tal1, int tal2)
+        {
+            int summa;
+            summa = tal1 + tal2;
+            Console.WriteLine("Nu skriver vi ut summan av de två talen här:" + summa);
+        }
+        static void Main(string[] args)
+        {
+            AdderaSumma(8, 2);
+        }
+    }
+}
