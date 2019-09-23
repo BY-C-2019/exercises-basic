8d6f5ac83587de31218cf58d9d0ffde242484192
wiseby <l.wiseby@gamil.com>
2019-09-23 09:02:16 +0200

    Exercise4 Done

---
+++ exercise4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] symbols = GetUnicodeCharacter();
+            foreach (var item in symbols)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.ReadKey();        
+        }
+
+        public static char[] GetUnicodeCharacter()
+        {
+            char[] symbols = new char[]
+            {
+                '\u0026',
+                '\u0029',
+                '\u0600',
+                '\u0603'
+            };
+            return symbols;
+        }
+    }
+}
