cceccd295a67f2c4b6f97427492ebd8142d47362
wiseby <l.wiseby@gamil.com>
2019-09-23 09:23:14 +0200

    Exercise6 Done

---
+++ exercise6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string answer;
+
+            do
+            {
+                Console.WriteLine("Nu ska vi se om det är fint väder!");
+                Console.WriteLine("Avsluta med [Q]");
+                Console.WriteLine("Är det fint väder?");
+                Console.Write("[J, N]>>");
+                input = Console.ReadLine();
+                answer = input.ToUpper();
+
+                if (answer == "J")
+                {
+                    Console.WriteLine("Vi går på picknick!");
+                    Console.ReadKey(); 
+                }
+                else if (answer == "N")
+                {
+                    Console.WriteLine("Vi stannar inne och läser en bok.");
+                    Console.ReadKey();
+                }
+                else if (answer == "Q")
+                {
+                    Console.WriteLine("Avslutar...");
+                    Console.ReadKey();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Jag förstår inte!?");
+                    Console.ReadKey();
+                }
+
+            }while(true);
+        }
+    }
+}
