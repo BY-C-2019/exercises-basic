fc18b72c50495997e9a66771dd98d5dcbd83c2fb
Filip Larsson <filip19911212@gmail.com>
2019-09-26 11:58:34 +0200

    method20

---
+++ method20/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercise20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in din ålder: ");
+            int age1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Age(age1));
+
+        }
+        static bool Age(int age)
+        {
+            Convert.ToBoolean(age);
+            if (age >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+
+        }
+    }
+}
