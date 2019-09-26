b9dc8adde35b4f020e5cb3f3cd14eb9287871a45
Filip Larsson <filip19911212@gmail.com>
2019-09-26 12:01:26 +0200

    method19

---
+++ method19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercise19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv in ett decimaltal: ");
+            double tal = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(tal + " avrundat blir: " + ToPrecentage(tal));
+        }
+        static double ToPrecentage(double tal)
+        {
+            double helTal = Math.Round(tal);
+            return helTal;
+        }
+    }
+}
